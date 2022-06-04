15b57a8486b46941e98a81940665e36788b820e6
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-04 19:57:50 +0530

    Adding a provider - path and a image sandwitch maker

--- app/src/main/java/com/example/drawingappforkids/MainActivity.kt
+++ app/src/main/java/com/example/drawingappforkids/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.drawingappforkids
 import android.Manifest
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -23,38 +26,31 @@ class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageButton? =
         null // A variable for current color is picked from color pallet.
 
-    val openGalleryLauncher: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-            if(result.resultCode == RESULT_OK && result.data!=null) {
-                val imageBackground: ImageView = findViewById(R.id.iv_background)
-                imageBackground.setImageURI(result.data?.data)
-            }
-        }
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
 
-    /** Todo 2: create an ActivityResultLauncher with MultiplePermissions since we are requesting
-     * both read and write
-     */
+        if (result.resultCode == RESULT_OK && result.data != null){
+            //process the data
+            val imageBackground:ImageView = findViewById(R.id.iv_background)
+            imageBackground.setImageURI(result.data?.data)
+        }
+    }
+
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             permissions.entries.forEach {
                 val perMissionName = it.key
                 val isGranted = it.value
-                //Todo 3: if permission is granted show a toast and perform operation
+                //if permission is granted show a toast and perform operation
                 if (isGranted ) {
                     Toast.makeText(
                         this@MainActivity,
                         "Permission granted now you can read the storage files.",
                         Toast.LENGTH_LONG
                     ).show()
-
-                    val pickIntent = Intent(Intent.ACTION_PICK,
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     openGalleryLauncher.launch(pickIntent)
-
-                    //perform operation
                 } else {
-                    //Todo 4: Displaying another toast if permission is not granted and this time focus on
+                    //Displaying another toast if permission is not granted and this time focus on
                     //    Read external storage
                     if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
                         Toast.makeText(
@@ -66,6 +62,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,17 +82,14 @@ class MainActivity : AppCompatActivity() {
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
-
-        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
-        ibUndo.setOnClickListener {
-            drawingView?.onClickUndo()
-        }
-
         val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
-        //TODO(Step 10 : Adding an click event to image button for selecting the image from gallery.)
-
         ibGallery.setOnClickListener {
             requestStoragePermission()
+        }
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+        ibUndo.setOnClickListener {
+            // This is for undo recent stroke.
+            drawingView?.onClickUndo()
         }
     }
 
@@ -152,21 +147,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //Todo 5: create a method to requestStorage permission
+    //create a method to requestStorage permission
     private fun requestStoragePermission(){
-        //Todo 6: Check if the permission was denied and show rationale
+        // Check if the permission was denied and show rationale
         if (
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
         ){
-            //Todo 9: call the rationale dialog to tell the user why they need to allow permission request
+            //call the rationale dialog to tell the user why they need to allow permission request
             showRationaleDialog("Kids Drawing App","Kids Drawing App " +
                     "needs to Access Your External Storage")
         }
         else {
             // You can directly ask for the permission.
-            // Todo 7: if it has not been denied then request for permission
+            //if it has not been denied then request for permission
             //  The registered ActivityResultCallback gets the result of this request.
             requestPermission.launch(
                 arrayOf(
@@ -177,7 +172,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    /** Todo 8: create rationale dialog
+    /**  create rationale dialog
      * Shows rationale dialog for displaying why the app needs permission
      * Only shown if the user has denied the permission request previously
      */
@@ -192,6 +187,32 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    // TODO(Step 4 : Getting and bitmap Exporting the image to your phone storage.)
+    /**
+     * Create bitmap from view and returns it
+     */
+    private fun getBitmapFromView(view: View): Bitmap {
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the specified width and height
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+        //Get the view's background
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        }
+        // draw the view on the canvas
+        view.draw(canvas)
+        //return the bitmap
+        return returnedBitmap
     }
 
 }
